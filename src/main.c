b758b4b28f13924b2eb2f28534ba3dd8aad28f34
olmoro <urk2t@yandex.ru>
2025-05-13 06:26:49 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+/**
+ * Проект mb_sp_12 as an inter-network gateway (межсетевой шлюз)
+ * 2025 май
+ */
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -17,42 +21,20 @@ static const char *TAG = "UART Gateway";
 // static QueueHandle_t uart1_queue, uart2_queue;
 // static SemaphoreHandle_t uart1_mutex, uart2_mutex;
 
-// // Структура для хранения Modbus (и Sp?) регистров
-// static uint16_t holding_regs[1] = {0};  // Holding регистры (16-bit)
-
 void app_main(void)
 {
-    // Инициализация NVS
-    //nvs_init_modbus_settings();
-
-    // ESP_ERROR_CHECK(init_nvs());
-    // ESP_LOGI(TAG, "NVS инициализирован");
-
-    // // Загрузка параметра из NVS
-    // int32_t stored_value = 0;
-    // if (load_parameter_from_nvs(&stored_value) == ESP_OK) 
-    // {
-    //     holding_regs[PARAM_REG_ADDR] = (uint16_t)stored_value;
-    //     ESP_LOGI(TAG, "Загружен параметр из NVS: %d", (int)stored_value);
-    // } 
-    // else 
-    // {
-    //     ESP_LOGW(TAG, "Параметр не найден, используется значение по умолчанию");
-    // }
-
-    /**   Инициализация NVS хранилища:
+    /** Инициализация NVS хранилища:
      * - очистка, если нет места или прошивается новая версия;
      * 
     */
     ESP_ERROR_CHECK(nvs_storage_init());
 
-    /**   Загрузка всех параметров из NVS
+    /** Загрузка всех параметров из NVS
      * - проверка версии прошивки;
-     * - сброс настроек если версия не найдена (первый запуск) или версия не совпадает с текущей;
+     * - сброс настроек если версия не найдена (первый запуск) 
+     *   или версия не совпадает с текущей;
      */
     ESP_ERROR_CHECK(load_all_parameters());
-
-
 
     // Инициализация периферии
     boardInit();
